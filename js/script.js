967e7a6486f24a102099a67865738a89fa37ad5c
Artyom Harutyunyan <artyom.pilot421@gmail.com>
2022-09-15 17:53:05 +0400

    commit

--- js/script.js
+++ js/script.js
@@ -25,17 +25,67 @@ function setActiveNavItem(id) {
     ul.classList.remove('visible')
 }
 
+function setActiveFirstTab(id) {
+
+    let firtsLi = document.getElementById('tab-1-first')
+    let secondLi = document.getElementById('tab-1-second')
+    let firstContent = document.getElementById('tabs-1-first-content')
+    let secondContent = document.getElementById('tabs-1-second-content')
+
+    if ( id === 'tab-1-first' ) {
+        firtsLi.classList.add('active')
+        secondLi.classList.remove('active')
+
+        firstContent.classList.remove('hidden')
+        secondContent.classList.add('hidden')
+
+    } else if ( id === 'tab-1-second' ) {
+        secondLi.classList.add('active')
+        firtsLi.classList.remove('active')
+
+        secondContent.classList.remove('hidden')
+        firstContent.classList.add('hidden')
+    }
+}
+
+function setActiveSecondTab(id) {
+
+    let secondTabLi = document.getElementsByClassName('second-tabs-li')
+    let secondTabContents = document.getElementsByClassName('second-tabs-content')
+
+    for ( let i = 1; i <= secondTabContents.length; i++ ) {
+        if ( i === id ) {
+            secondTabLi[i-1].classList.add('active')
+            secondTabContents[i-1].classList.remove('hidden')
+        }else {
+            secondTabLi[i-1].classList.remove('active')
+            secondTabContents[i-1].classList.add('hidden')
+        }
+    }
+}
+
+function isActivThidrTab(id) {
+
+    let thirdTabLi = document.getElementsByClassName('third-tab-li')
+    let thirdTabContents = document.getElementsByClassName('third-tab-content')
+
+    for ( let i = 1; i <= thirdTabContents.length; i++ ) {
+        if ( i === id ) {
+            thirdTabLi[i-1].classList.add('active')
+            thirdTabContents[i-1].classList.remove('hidden')
+        }else {
+            thirdTabLi[i-1].classList.remove('active')
+            thirdTabContents[i-1].classList.add('hidden')
+        }
+    }
+}
+
+
 $(function() {
 
     $('#menu-btn').click(function() {
        $('#new-menu-ul').toggleClass('visible') 
     })
-
-    $( "#tabs" ).tabs();
-
-    $( "#second-tabs" ).tabs();
-    
-    $( "#third-tabs" ).tabs();
     
     $('.jcarousel').jcarousel().jcarouselAutoscroll({
         interval: 3000,
